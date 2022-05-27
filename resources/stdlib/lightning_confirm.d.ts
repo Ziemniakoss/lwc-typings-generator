8b3a9ad7fc2ed3e7cf71b1af8ec89d2011bb0af2
Przemek <rawa.przemek@gmail.com>
2022-05-27 21:20:44 +0200

    Add support for confiramtion api

---
+++ resources/stdlib/lightning_confirm.d.ts
@@ -0,0 +1,24 @@
+declare module "lightning/confirm" {
+	type ConfirmationVariant = "headerless"
+	type ConfirmationTheme = "default" | "error" | "warning" | "info"
+
+	interface LightningConfirmationConfig {
+		message: string
+		/**
+		 * Can be header less or with header if no value is provided
+		 */
+		variant?: ConfirmationVariant
+		theme?: ConfirmationTheme
+		/**
+		 * Displayed in header (in variant with header)
+		 */
+		label: string
+	}
+
+	interface LightningConfirmationType {
+		open: (config: LightningConfirmationConfig) => Promise<boolean>
+	}
+
+	const LightningConfirm: LightningConfirmationType
+	export default LightningConfirm
+}
---
+++ resources/stdlib/lightning_prompt.d.ts
@@ -0,0 +1,19 @@
+declare module "lightning/prompt" {
+	type PromptTheme = "default" | "error" | "warning" | "info"
+	interface LightningPromptConfig {
+		message:string
+		label:string
+		theme?:PromptTheme
+		defaultValue?:string
+	}
+	interface LightningPromptType {
+		/**
+		 *
+		 * @param config
+		 * @return null if user clicked cancel
+		 */
+		open:(config:LightningPromptConfig) => Promise<string | null>
+	}
+	const LightningPrompt: LightningPromptType
+	export default LightningPrompt
+}
