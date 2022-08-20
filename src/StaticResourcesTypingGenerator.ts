323dd43d3be7c83439423e71455bea2ada470991
Przemek <rawa.przemek@gmail.com>
2022-08-20 22:15:49 +0200

    fix static resources iteration

--- src/StaticResourcesTypingGenerator.ts
+++ src/StaticResourcesTypingGenerator.ts
@@ -1,8 +1,9 @@
-import { Connection } from "jsforce";
+import {Connection, FileProperties} from "jsforce";
 import { SfdxProject } from "@salesforce/core";
 import { getTypingsDir } from "./utils/filesUtils";
 import { join } from "path";
 import { promises } from "fs";
+import {wrapInArray} from "./utils/collectionUtils";
 
 export default class StaticResourcesTypingGenerator {
 	async generateTypingsForProject(
@@ -10,13 +11,14 @@ export default class StaticResourcesTypingGenerator {
 		project: SfdxProject
 	) {
 		const [staticResources, typingsFolder] = await Promise.all([
-			connection.metadata.list({ type: "StaticResource" }),
+			this.fetchStaticResources(connection),
 			getTypingsDir(project),
 		]);
 		const typingsFile = join(typingsFolder, "static-resources.d.ts");
 		await promises.writeFile(typingsFile, "");
 
 		for (const staticResource of staticResources) {
+			console.log(staticResource.fullName)
 			const fullName = staticResource.fullName;
 			await promises.appendFile(
 				typingsFile,
@@ -27,5 +29,12 @@ export default class StaticResourcesTypingGenerator {
 			}\n\n`
 			);
 		}
+	}
+
+	private async fetchStaticResources(connection:Connection):Promise<FileProperties[]> {
+		return connection
+			.metadata
+			.list({ type: "StaticResource" })
+			.then(staticResources => wrapInArray(staticResources))
 	}
 }
