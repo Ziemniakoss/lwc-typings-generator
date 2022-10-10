a1c84d2df0d909de68e29282b09dd202c6443c3a
Przemek <rawa.przemek@gmail.com>
2022-10-10 18:52:07 +0200

    fix generation for SObjects without record types

--- src/sObjectTypingsGeneration/SObjectTypingsGenerator.ts
+++ src/sObjectTypingsGeneration/SObjectTypingsGenerator.ts
@@ -3,6 +3,7 @@ import { join } from "path";
 import { promises } from "fs";
 import { mkdirs } from "../utils/filesUtils";
 import IFieldTypingsGeneratorFactory from "./IFieldTypingsGeneratorFactory";
+import { wrapInArray } from "../utils/collectionUtils";
 
 export default class SObjectTypingsGenerator {
 	constructor(
@@ -55,10 +56,16 @@ export default class SObjectTypingsGenerator {
 	}
 
 	generateRecordTypesTypings(describe: DescribeSObjectResult): string {
-		let devNamesMerged = describe.recordTypeInfos
+		const typeDefinition = `\ttype ${describe.name}__RecordType__DevName =`;
+
+		const recordTypeInfos = wrapInArray(describe.recordTypeInfos);
+		if (recordTypeInfos.length == 0) {
+			return typeDefinition + " any;\n\n";
+		}
+		const devNamesMerged = describe.recordTypeInfos
 			.map((rtInfo) => rtInfo.developerName)
-			.map((devName) => `"${devName}"`)
-			.join(" | ");
-		return `type ${describe.name}__RecordType__DevName = ${devNamesMerged};\n\n`;
+			.map((devName) => `\t\t"${devName}"`)
+			.join(" |\n");
+		return typeDefinition + "\n" + devNamesMerged + ";\n\n";
 	}
 }
