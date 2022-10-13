0fdce3f4271035f8ba9b0bec53c05d4a98dd9036
Ziemniakoss <48589406+Ziemniakoss@users.noreply.github.com>
2022-10-13 20:47:32 +0200

    bugfix/fix-typings
    * fix typings for ShowToastEvent
    
    * fix typings for disptachEvent
    
    * fix typings for flow support events

--- resources/stdlib/lightning_platformShowToastEvent.d.ts
+++ resources/stdlib/lightning_platformShowToastEvent.d.ts
@@ -2,8 +2,8 @@ declare module "lightning/platformShowToastEvent" {
 	interface ToastEventConfig {
 		title: string;
 		message: string;
-		variant: "success" | "info" | "warning" | "error";
-		mode: "dismissible" | "pester" | "sticky";
+		variant?: "success" | "info" | "warning" | "error";
+		mode?: "dismissible" | "pester" | "sticky";
 	}
 
 	export class ShowToastEvent extends CustomEvent {
