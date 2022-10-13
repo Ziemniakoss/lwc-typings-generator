454be8bf75bdd71c8e42afbef4b8518ffc241231
Przemek <rawa.przemek@gmail.com>
2022-10-13 20:46:05 +0200

    fix typings for flow support events

--- resources/stdlib/lightning_flowSupport.d.ts
+++ resources/stdlib/lightning_flowSupport.d.ts
@@ -1,25 +1,33 @@
 declare module "lightning/flowSupport" {
-	class FlowAttributeChangeEvent extends CustomEvent {
+	class FlowAttributeChangeEvent extends CustomEvent<any> {
 		constructor(attributeName: string, value);
 	}
 
 	/**
 	 * Requests flow to go to previous screen
 	 */
-	class FlowNavigationBackEvent extends CustomEvent {}
+	class FlowNavigationBackEvent extends CustomEvent<any> {
+		constructor();
+	}
 
 	/**
 	 * Requests flow to go to next screen
 	 */
-	class FlowNavigationNextEvent extends CustomEvent {}
+	class FlowNavigationNextEvent extends CustomEvent<any> {
+		constructor();
+	}
 
 	/**
 	 * Requests runtime to pause flow
 	 */
-	class FlowNavigationPauseEvent extends CustomEvent {}
+	class FlowNavigationPauseEvent extends CustomEvent<any> {
+		constructor();
+	}
 
 	/**
 	 * Requests runtime to terminate flow
 	 */
-	class FlowNavigationFinishEvent extends CustomEvent {}
+	class FlowNavigationFinishEvent extends CustomEvent<any> {
+		constructor();
+	}
 }
