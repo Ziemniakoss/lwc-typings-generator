81f5de2273a0f17de4e7f025c7f03aea4fa9f219
Przemek <rawa.przemek@gmail.com>
2022-10-23 21:00:45 +0200

    format with prettier

--- resources/stdlib/lightning_refresh.d.ts
+++ resources/stdlib/lightning_refresh.d.ts
@@ -1,12 +1,12 @@
 //TODO more docs when released
 declare module "lightning/refresh" {
-	export const REFRESH_COMPLETE: symbol
+	export const REFRESH_COMPLETE: symbol;
 
-	export const REFRESH_COMPLETE_WITH_ERRORS: symbol
+	export const REFRESH_COMPLETE_WITH_ERRORS: symbol;
 
-	export const REFRESH_ERROR: symbol
+	export const REFRESH_ERROR: symbol;
 
-	export const RefreshEventName: "lightning__refresh"
+	export const RefreshEventName: "lightning__refresh";
 
 	//TODO better typings when docs are released
 	export function registerRefreshContainer(...args);
@@ -19,6 +19,6 @@ declare module "lightning/refresh" {
 
 	//TODO better typings when docs are released
 	export class RefreshEvent extends CustomEvent {
-		constructor(...args)
+		constructor(...args);
 	}
 }
