155ff7d183db15ce81ac3e20eb468bbf51b71abf
Ziemniakoss <48589406+Ziemniakoss@users.noreply.github.com>
2022-10-23 21:45:13 +0200

    feature/basic-refresh-api-support
    * basic typings for new
    
    * format with prettier
    
    * fx typings for isGuest

---
+++ resources/stdlib/lightning_refresh.d.ts
@@ -0,0 +1,24 @@
+//TODO more docs when released
+declare module "lightning/refresh" {
+	export const REFRESH_COMPLETE: symbol;
+
+	export const REFRESH_COMPLETE_WITH_ERRORS: symbol;
+
+	export const REFRESH_ERROR: symbol;
+
+	export const RefreshEventName: "lightning__refresh";
+
+	//TODO better typings when docs are released
+	export function registerRefreshContainer(...args);
+
+	//TODO better typings when docs are released
+	export function registerRefreshHandler(...args);
+
+	//TODO better typings when docs are released
+	export function unregisterRefreshContainer(...args);
+
+	//TODO better typings when docs are released
+	export class RefreshEvent extends CustomEvent {
+		constructor(...args);
+	}
+}
