a060b62fc2cd549ce622bb0da91a2dcc12d7b97b
Przemek <rawa.przemek@gmail.com>
2022-10-24 23:25:25 +0200

    add support for lightning/actions module

---
+++ resources/stdlib/lightning_actions.d.ts
@@ -0,0 +1,11 @@
+/**
+ * [Full docs](https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.use_quick_actions_screen)
+ */
+declare module "lightning/actions" {
+	/**
+	 * Closes action represented by current component
+	 */
+	export class CloseActionScreenEvent extends CustomEvent {
+		constructor();
+	}
+}
