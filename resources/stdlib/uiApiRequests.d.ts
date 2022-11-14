401400aaacd39bffba4673cc487f8d9718c42937
Przemek <rawa.przemek@gmail.com>
2022-11-14 19:44:11 +0100

    Fix types for uiApiRequests.RecordInput
    apiName is not required

--- resources/stdlib/uiApiRequests.d.ts
+++ resources/stdlib/uiApiRequests.d.ts
@@ -1,6 +1,9 @@
 declare module uiApiRequests {
 	declare interface RecordInput<T extends schema.SObjectApiName> {
-		apiName: T;
+		/**
+		 * For updates this field is not required
+		 */
+		apiName?: T;
 		fields: Partial<Record<keyof schema.SObjectsMap[T], any>>;
 	}
 }
